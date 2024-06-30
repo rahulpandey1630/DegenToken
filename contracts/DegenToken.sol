c6fa5484e312ca4a85bbc65b39e17e4fdc9b561a
Rahul Kumar Pandey <rahulpandey1630@gmail.com>
2024-06-30 18:00:40 +0000

    initial commit

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,58 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenGamingToken is Ownable, ERC20Burnable {
+    mapping(uint256 => uint256) public opitionToPrice; // 1: 50 DGTs , 2: 200 DGTs ....
+    mapping(address => uint256[]) public addressToRewards; // 0xasdfc : [1,2,2] ...
 
-    constructor() ERC20("Degen", "DGN") {}
+    constructor() ERC20("DegenGamingToken", "DGT") Ownable(msg.sender) {
+        opitionToPrice[1] = 50;
+        opitionToPrice[2] = 200;
+        opitionToPrice[3] = 5000;
+    }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function showRewards() public pure returns (string memory) {
+        string memory rewardList = "1: Basic Power Boost - 50 DGTs \n2: Advance Armor Set - 200 DGTs\n3: Legendary Weapon - 5000 DGTs";
+        console.log(rewardList);
+        return rewardList;
+    }
+
+    function mintToken(address _to, uint256 _value) external onlyOwner {
+        _mint(_to, _value);
+    }
+
+    function transferToken(address _to, uint256 _value) public {
+        require(balanceOf(msg.sender) >= _value, "You don't have enough DGTs");
+        approve(msg.sender, _value);
+        transferFrom(msg.sender, _to, _value);
+    }
+
+    function redeemAwards(uint256 _optionNumber) public {
+        require(
+            opitionToPrice[_optionNumber] != 0,
+            "Please select a valid option"
+        );
+        require(
+            balanceOf(msg.sender) >= opitionToPrice[_optionNumber],
+            "You don't have engouh DGTs"
+        );
+        burn(opitionToPrice[_optionNumber]);
+        addressToRewards[msg.sender].push(_optionNumber);
+    }
+
+    function checkBalance() public view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function burnToken(uint256 _value) public {
+        burn(_value);
+    }
+
+    function showRedeemedRewards(address _player) public view returns(uint[] memory){
+        return addressToRewards[_player];
     }
 }
